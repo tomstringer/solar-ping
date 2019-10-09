71b7e23c1f8297a0a4a544cb545762251c6fd93c
tomstringer <t.stringer@unimelb.edu.au>
2019-10-09 15:20:03 +1100

    Initial setup

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'bundler'
+Bundler.setup
+require 'sinatra'
+require 'json'
+require 'rest-client'
+
+get '/' do
+  api_result = RestClient.get 'https://api.enphaseenergy.com/api/v2/systems/1306036/rgm_stats?key=bffbc4f8c406fb68b7a0eb2290a5cd03&user_id=4f546b324d44637a0a?datetime_format=iso8601'
+  jhash = JSON.parse(api_result)
+  output = ''
+
+  jhash['main'].each do |w|
+    title_tag = w[0]
+  info_item = w[1]
+    output << "<tr><td>#{system_id}</td><td>#{total_devices}</td></tr>"
+end
+
+  erb :index, :locals => {results: output}
+end
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 - http://tutorials.jumpstartlab.com/projects/web_guesser.html
 - http://webapps-for-beginners.rubymonstas.org/sinatra.html
+- https://www.engineyard.com/blog/doing-an-api-mashup-with-sinatra
