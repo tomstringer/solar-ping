d082a4dbfb5f9e2818b1e8938e0850ce91411fc2
tomstringer <t.stringer@unimelb.edu.au>
2019-10-10 13:30:17 +1100

    Bunch o lil changes

--- app.rb
+++ app.rb
@@ -4,17 +4,18 @@ Bundler.setup
 require 'sinatra'
 require 'json'
 require 'rest-client'
+require 'sinatra/reloader' if development?
 
 get '/' do
   api_result = RestClient.get 'https://api.enphaseenergy.com/api/v2/systems/1306036/rgm_stats?key=bffbc4f8c406fb68b7a0eb2290a5cd03&user_id=4f546b324d44637a0a?datetime_format=iso8601'
   jhash = JSON.parse(api_result)
   output = ''
 
-  jhash['main'].each do |w|
+  jhash['meta'].each do |w|
     title_tag = w[0]
-  info_item = w[1]
-    output << "<tr><td>#{system_id}</td><td>#{total_devices}</td></tr>"
-end
+  	info_item = w[1]
+    output << "<tr><td>#{status}</td><td></td></tr>"
+	end
 
   erb :index, :locals => {results: output}
 end
