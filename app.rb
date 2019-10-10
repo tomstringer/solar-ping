10f8f990d5757859a6b6be79dce81389de1db29f
tomstringer <t.stringer@unimelb.edu.au>
2019-10-10 13:32:55 +1100

    Update app.rb

--- app.rb
+++ app.rb
@@ -7,9 +7,9 @@ require 'rest-client'
 require 'sinatra/reloader' if development?
 
 get '/' do
-  api_result = RestClient.get 'https://api.enphaseenergy.com/api/v2/systems/1306036/rgm_stats?key=bffbc4f8c406fb68b7a0eb2290a5cd03&user_id=4f546b324d44637a0a?datetime_format=iso8601'
-  jhash = JSON.parse(api_result)
-  output = ''
+	api_result = RestClient.get 'https://api.enphaseenergy.com/api/v2/systems/1306036/rgm_stats?key=bffbc4f8c406fb68b7a0eb2290a5cd03&user_id=4f546b324d44637a0a?datetime_format=iso8601'
+	jhash = JSON.parse(api_result)
+	output = ''
 
 	jhash['meta'].each do |w|
 		title_tag = w[0]
